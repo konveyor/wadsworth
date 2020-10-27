e53b493ea93f454a5bfb6d7f1d6c67ed03d2f992
Erik Nelson <erik@nsk.io>
2020-10-27 17:24:21 -0400

    Add updates to jira-client
    Subtask creation and fetching issues

--- jira-client.js
+++ jira-client.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
+const common = require('./common');
 
 const JIRA_API_ROOT = 'https://issues.redhat.com'
+const PROJECT_KEY = 'MIG';
+const SUBTASK_TYPE_ID = '5';
+const ISSUE_PATH = '/rest/api/2/issue';
+
+const issuePathForId = (issueId) => `${ISSUE_PATH}/${issueId}`;
 
 class JiraClient {
   constructor(jiraUser, jiraPass) {
@@ -17,15 +23,51 @@ class JiraClient {
     })
   }
 
-  async FetchSubtasks(jiraIssueId) {
-    console.log(`JiraClient::FetchSubtasks(${jiraIssueId})`);
-    const res = await this.requester.get(`/rest/api/2/issue/${jiraIssueId}`);
+  async FetchIssue(jiraIssueId) {
+    console.log(`JiraClient::Issue(${jiraIssueId})`);
+    const res = await this.requester.get(issuePathForId(jiraIssueId));
     return res.data;
   }
 
-  async AddSubtask(jiraIssueId, githubIssue) {
-    console.log('JiraClient::AttachSubtask');
+  async AddSubtaskForGithubIssue(jiraIssueId, githubIssue) {
+    const jit = GetJiraIssueTag(githubIssue);
+    console.log(`JiraClient::AddSubtaskForGithubIssue - ${jiraIssueId} - ${jit}`);
+
+    const parsedTitle = common.ParseGHITitle(githubIssue);
+    const subtaskTitle = `${jit}: ${parsedTitle.trimmedTitle}`;
+
+    console.log('Creating subtask with title:')
+    console.log(subtaskTitle);
+
+    const subtaskReqBody = createSubtaskReqBody(
+      subtaskTitle, githubIssue.issue.html_url, jiraIssueId);
+    const res = await this.requester.post(ISSUE_PATH, subtaskReqBody);
+
+    console.log('Got result:');
+    console.log(res);
+
+    return res.data;
   }
 }
 
-module.exports = JiraClient;
+function createSubtaskReqBody(summary, description, parentId) {
+  return {
+    fields: {
+      project: {
+        key: PROJECT_KEY,
+      },
+      parent: {
+        key: parentId,
+      },
+      summary,
+      description,
+      issuetype: { id: SUBTASK_TYPE_ID },
+    }
+  }
+}
+
+function GetJiraIssueTag(ghIssue){
+  return `[${ghIssue.repository.name}#${ghIssue.issue.number}]`;
+}
+
+module.exports = { JiraClient, GetJiraIssueTag };
