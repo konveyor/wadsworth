8a71d6d2cb9485b27b11a38882322b65eaa40ef3
Erik Nelson <erik@nsk.io>
2020-10-29 12:21:47 -0400

    Remove sample gh issue

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ~/.secrets/jira-creds
-export GH_ISSUE_FILE="$_dir/../sample-gh-issue.txt"
-JIRA_USER="${JIRA_USER}" JIRA_PASS="${JIRA_PASS}" node main.js
+JIRA_USER="${JIRA_USER}" JIRA_PASS="${JIRA_PASS}" node $_dir/../main.js
