2419ebbe1f1ca2fdd17af6ac6bd87626fd190a70
Erik Nelson <erik@nsk.io>
2020-10-29 15:14:15 -0400

    sync

--- scripts/testjira.sh
+++ scripts/testjira.sh
@@ -2,22 +2,23 @@
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ~/.secrets/jira-creds
 jirahost='issues.redhat.com'
-#jiraid="MIG-357"
-jiraid="MIG-365"
+jiraid="MIG-357"
+#jiraid="MIG-365"
 token=$(echo "$JIRA_USER:$JIRA_PASS" | base64)
 
-#curl \
-  #-X GET \
-  #-H "Authorization: Basic $token" \
-  #-H "Content-Type: application/json" \
+curl \
+  -X GET \
+  -H "Authorization: Basic $token" \
+  -H "Content-Type: application/json" \
+  "https://$jirahost/rest/api/2/issue/$jiraid"
 
 #curl -X GET \
   #-H "Authorization: Basic $token" \
   #-H "Content-Type: application/json" \
   #"https://$jirahost/rest/api/2/issue/$jiraid/transitions"
 
-curl -X POST \
-  --data @$_dir/test.json \
-  -H "Authorization: Basic $token" \
-  -H "Content-Type: application/json" \
-  "https://$jirahost/rest/api/2/issue/$jiraid/transitions"
+#curl -X POST \
+  #--data @$_dir/test.json \
+  #-H "Authorization: Basic $token" \
+  #-H "Content-Type: application/json" \
+  #"https://$jirahost/rest/api/2/issue/$jiraid/transitions"
